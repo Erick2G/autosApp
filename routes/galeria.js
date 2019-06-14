11afb065ba078e281a5c31217ba7790a6f0d47a0
Erick2G <e2g369@gmail.com>
2019-06-14 11:57:19 -0500

    Se agrega la ruta /maza /dodge /galeria/mazda

---
+++ routes/galeria.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/mazda',function(req,res,next){
+	var data={autos:[]};
+	var auto={};
+	auto.nombre="Mazda 3  ";
+	auto.foto="https://www.mazda.mx/siteassets/mazda-mx/mycos-2019/mazda-3-sedan/vlp/versiones/mazda-3-sedan-vlp-versiones-i.jpg"
+	data.autos.push(auto);
+
+	auto=null;
+	var auto={};
+	auto.nombre="Mazda 6  ";
+	auto.foto="https://www.romakurumamazda.com/wp-content/uploads/2018/04/MAZDA6_GRANDE.jpg"
+	data.autos.push(auto);
+
+	auto=null;
+	var auto={};
+	auto.nombre="Mazda cx3";
+	auto.foto="https://www.coches.com/fotos_historicas/mazda/CX-3/high_318b730d19545c6351f312d234cb153c.jpg"
+	data.autos.push(auto);
+
+	auto=null;
+	var auto={};
+	auto.nombre="Mazda cx3";
+	auto.foto="https://www.coches.com/fotos_historicas/mazda/CX-3/high_318b730d19545c6351f312d234cb153c.jpg"
+	data.autos.push(auto);
+
+	auto=null;
+	var auto={};
+	auto.nombre="Mazda cx3";
+	auto.foto="https://www.coches.com/fotos_historicas/mazda/CX-3/high_318b730d19545c6351f312d234cb153c.jpg"
+	data.autos.push(auto);
+
+	console.log(data);
+
+	res.render("./galeria/mazda",data);
+});
+
+router.get('/yisus',function(req,res,next){
+	res.render('prueba_yisus');//el nombre de un view terminacion hbs
+});
+
+module.exports =router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'autosApp' });
+});
+module.exports = router;
+router.get('/mazda',function(req,res,next){
+	var infoMazda={};
+	infoMazda.pais='Yisus help us';
+	infoMazda.sectorVentas='Hola que hace';
+	infoMazda.anioFundacion='1929';
+	infoMazda.logo='https://www.mazda.mx/siteassets/mazda-mx/logos-new-mazda/mazda-logo-desktop-2.png';
+	res.render('mazda',infoMazda);
+});
+router.get('/dodge',function(req,res,next){
+	var infoDodge={};
+	infoDodge.pais='United States';
+	infoDodge.sectorVentas='Comercial';
+	infoDodge.anioFundacion='-- Desde 1900 --';
+	infoDodge.logo='https://s3.amazonaws.com/comparamejor-wp/uploads/2016/03/Dodge-LOGO-1.png';
+	res.render('dodge',infoDodge);
 });
 
-module.exports = router;
